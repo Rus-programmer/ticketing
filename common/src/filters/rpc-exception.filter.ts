535beb4e84e633a798685a9d5655cf4fbbb98d64
rus <math.it.sport@gmail.com>
2025-01-05 13:55:52 +0500

    add rpc logging

--- common/src/filters/rpc-exception.filter.ts
+++ common/src/filters/rpc-exception.filter.ts
@@ -1,10 +1,17 @@
 import { Catch, RpcExceptionFilter, ArgumentsHost } from '@nestjs/common';
 import { Observable, throwError } from 'rxjs';
 import { RpcException } from '@nestjs/microservices';
+import { LoggerService } from '../utils/logger/logger.service';
 
 @Catch(RpcException)
 export class RpcExFilter implements RpcExceptionFilter<RpcException> {
+  constructor(private logger: LoggerService) {
+    logger.setContext(RpcExFilter.name);
+  }
+
   catch(exception: RpcException, host: ArgumentsHost): Observable<any> {
-    return throwError(() => exception.getError());
+    const error = exception.getError();
+    this.logger.error(error);
+    return throwError(() => error);
   }
 }
